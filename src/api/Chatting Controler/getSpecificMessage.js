741903fc23c341576a22244d56d9c2ca485384f9
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-01-15 02:41:16 +0600

    chat messageing data almos done here

--- src/api/Chatting Controler/getSpecificMessage.js
+++ src/api/Chatting Controler/getSpecificMessage.js
@@ -4,9 +4,7 @@ const Message = require("../../model/Message/Message Model");
 const getSpecificMessage = async (req, res) => {
   try {
     const id = req.params._id;
-    const messages = await Message.find({ conversationId: id }).sort(
-      "-createdAt"
-    );
+    const messages = await Message.find({ conversationId: id });
     const { participant } = await Conversation.findById(id);
     res.send({ message: messages, participant });
   } catch (error) {
